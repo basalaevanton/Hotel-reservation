4ef4893cd7ebec73845a3b93bc1d71ba4526a5c2
basalaevanton <74416144+basalaevanton@users.noreply.github.com>
2021-08-25 19:15:54 +0300

    component input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,52 @@
-import { Button, Htag, P } from '../components';
+import { Button, Htag, P, Input } from '../components';
 import type { NextPage } from 'next';
 import 'normalize.css';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          textAlign: 'center',
-          width: '100%',
-          height: '100vh',
-        }}
-      >
+    <>
+      <div>
         <div
           style={{
-            width: '90vw',
-            maxWidth: '500px',
-            border: '1px solid gray',
-            borderRadius: '5px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            width: '100%',
+            height: '100vh',
           }}
         >
-          <h1> NextJs app...</h1>
-          <h2>Hello World!</h2>
-          <Button appearance="primary">click on me</Button>
-          <Button appearance="ghost">click on me</Button>
-          <Button border="primary">click on me</Button>
-          <Button border="ghost">click on me</Button>
-          <Button unactive>click on me</Button>
-          <Button appearance="primary" arrow="right">
-            click on me
-          </Button>
-          <Htag tag="h1">This one is the sub-section or widget title </Htag>
-          <Htag tag="h2">This one is the sub-section or widget title </Htag>
-          <Htag tag="h3">This one is the sub-section or widget title </Htag>
-          <P>
-            This is the body text which is used for most of the design, like
-            paragraphs, lists, etc.
-          </P>
+          <div
+            style={{
+              width: '100%',
+              maxWidth: '500px',
+              border: '1px solid gray',
+              borderRadius: '5px',
+            }}
+          >
+            <h1> NextJs app...</h1>
+            <h2>Hello World!</h2>
+            <Button appearance="primary">click on me</Button>
+            <Button appearance="ghost">click on me</Button>
+            <Button border="primary">click on me</Button>
+            <Button border="ghost">click on me</Button>
+            <Button unactive>click on me</Button>
+            <Button appearance="primary" arrow="right">
+              click on me
+            </Button>
+            <Htag tag="h1">This one is the sub-section or widget title </Htag>
+            <Htag tag="h2">This one is the sub-section or widget title </Htag>
+            <Htag tag="h3">This one is the sub-section or widget title </Htag>
+            <P>
+              This is the body text which is used for most of the design, like
+              paragraphs, lists, etc.
+            </P>
+            <Input placeholder="Email" arrow="right" />
+            <Input type="date" placeholder="Email" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
