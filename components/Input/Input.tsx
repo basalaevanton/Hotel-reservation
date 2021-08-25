daf0fa5c83825e564779e92a1ba521427871896e
basalaevanton <74416144+basalaevanton@users.noreply.github.com>
2021-08-25 19:17:05 +0300

    component input

---
+++ components/Input/Input.tsx
@@ -0,0 +1,27 @@
+import { InputProps } from './Input.props';
+import styles from './Input.module.scss';
+import ArrowIcon from './arrow.svg'
+
+import cn from 'classnames';
+import { ForwardedRef, forwardRef } from 'react';
+
+export const Input = forwardRef(
+  (
+    { className, arrow, error, ...props }: InputProps,
+    ref: ForwardedRef<HTMLInputElement>
+  ): JSX.Element => {
+    return (
+      <div className={cn(className, styles.inputWrapper)}>
+        <input
+          className={cn(styles.input, {
+            [styles.error]: error,
+          })}
+          ref={ref}
+          {...props}
+        />
+        {arrow && <ArrowIcon className={styles.arrowIcon}/>}
+        {error && <span className={styles.errorMessage}>{error.message}</span>}
+      </div>
+    );
+  }
+);
